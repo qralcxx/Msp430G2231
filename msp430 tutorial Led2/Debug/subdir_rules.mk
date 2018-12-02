08a2ecc6bf3e983407dc969e529816abac0e878f
qralcxx <38606171+qralcxx@users.noreply.github.com>
2018-12-02 15:04:04 +0300

    Add files via upload

--- msp430 tutorial Led2/Debug/subdir_rules.mk
+++ msp430 tutorial Led2/Debug/subdir_rules.mk
@@ -8,7 +8,7 @@ SHELL = cmd.exe
 %.obj: ../%.c $(GEN_OPTS) | $(GEN_FILES)
 	@echo 'Building file: "$<"'
 	@echo 'Invoking: MSP430 Compiler'
-	"C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccsv8/ccs_base/msp430/include" --include_path="C:/Users/Monster/Desktop/Yeni klasör (2)/msp430 tutorial Led2" --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include" --advice:power=all --define=__MSP430G2231__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
+	"C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/bin/cl430" -vmsp --use_hw_mpy=none --include_path="C:/ti/ccsv8/ccs_base/msp430/include" --include_path="C:/Users/Monster/workspace_v8/msp430 tutorial Led2" --include_path="C:/ti/ccsv8/tools/compiler/ti-cgt-msp430_18.1.3.LTS/include" --advice:power=all --define=__MSP430G2231__ -g --printf_support=minimal --diag_warning=225 --diag_wrap=off --display_error_number --preproc_with_compile --preproc_dependency="$(basename $(<F)).d_raw" $(GEN_OPTS__FLAG) "$<"
 	@echo 'Finished building: "$<"'
 	@echo ' '
 
--- msp430 tutorial Led2/main.c
+++ msp430 tutorial Led2/main.c
@@ -1,12 +1,17 @@
 #include <msp430.h> 
 
-
-/**
- * main.c
- */
 int main(void)
 {
-	WDTCTL = WDTPW | WDTHOLD;	// stop watchdog timer
-	
+	WDTCTL = WDTPW | WDTHOLD;	        // stop watchdog timer
+	volatile  int i = 0;                // variable for delay
+
+	P1DIR |=BIT0;                       // sets BIT0 output
+
+	while(1){                           //loop
+	    P1OUT &=~BIT0;                  //light up BIT0
+	    for(i=0;i<20000;i++);       //delay
+	    P1OUT |=BIT0;                   //light down BIT0
+	    for(i=0;i<20000;i++);       //delay
+	}
 	return 0;
 }
